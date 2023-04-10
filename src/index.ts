568522edd981be350597f325890c490811832adc
Gabriel Sánchez <gabionezero@gmail.com>
2023-04-10 10:31:41 +0200

    Add config details

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,16 @@ dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT;
+const cors = require('cors')
+const whitleList = ['http://localhost:4200']
+
+app.use(express.json())
+app.use(cors({origin: whitleList}))
 
 app.get('/ping', (_req: Request, res: Response) => {
   res.send('Pong!');
 });
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️[SERVER]: Server is running at http://localhost:${PORT}`);
 });
